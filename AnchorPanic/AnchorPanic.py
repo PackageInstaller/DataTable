c2953b1c2b8006ee4ca35d5a17d66cff1de2a2f8
PackageInstaller <yjzyl9008@gmail.com>
2025-08-14 20:15:58 +0800

    StarResonance 数据表

--- AnchorPanic/AnchorPanic.py
+++ AnchorPanic/AnchorPanic.py
@@ -1,52 +1,38 @@
 import hashlib
 from Crypto.Cipher import DES
 from Crypto.Util.Padding import unpad
-import argparse
 import os
+from sys import argv
 
-IV_HEX = "6F9732CD7BDEB92D"
-IV_BYTES = bytes.fromhex(IV_HEX)
 
 def get_des_key():
-    initial_string = "qdiazawh"
 
     md5_hasher = hashlib.md5()
-    md5_hasher.update(initial_string.encode('utf-8'))
+    md5_hasher.update("qdiazawh".encode('utf-8'))
     md5_hex_string = md5_hasher.hexdigest()
     common_key_string = md5_hex_string[-8:]
-    des_key_bytes = common_key_string.encode('utf-8')
-    return des_key_bytes
 
-def decrypt_file(encrypted_file_path, des_key_bytes, iv_bytes):
-    try:
-        cipher = DES.new(des_key_bytes, DES.MODE_CBC, iv_bytes)
+    return common_key_string.encode('utf-8')
 
-        with open(encrypted_file_path, 'rb') as f_enc:
-            encrypted_data = f_enc.read()
+def dec(path, key, iv):
 
-        decrypted_padded_data = cipher.decrypt(encrypted_data)
-        decrypted_data = unpad(decrypted_padded_data, DES.block_size)
+    cipher = DES.new(key, DES.MODE_CBC, iv)
 
-        with open(encrypted_file_path, 'wb') as f_dec:
-            f_dec.write(decrypted_data)
+    with open(path, 'rb') as f_enc:
+        enc= f_enc.read()
 
-        return True
+    with open(path, 'wb') as f_dec:
+        f_dec.write(unpad(cipher.decrypt(enc), DES.block_size))
 
-    except ValueError:
-        return False
 
-def decrypt_directory(directory_path):
+def decrypt(path):
     des_key = get_des_key()
     
-    for root, _, files in os.walk(directory_path):
+    for root, _, files in os.walk(path):
         for file in files:
             if file.endswith('.lua'):
                 file_path = os.path.join(root, file)
-                decrypt_file(file_path, des_key, IV_BYTES)
+                dec(file_path, des_key, bytes.fromhex("6F9732CD7BDEB92D"))
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("i")
-    args = parser.parse_args()
-
-    decrypt_directory(args.i)
+    decrypt(argv[1])
